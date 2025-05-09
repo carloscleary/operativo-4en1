8861ec5908d397c1867355a89d31262bd4ec43c8
carloscleary <58497957+carloscleary@users.noreply.github.com>
2025-05-09 10:58:57 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
       <h1>Operativo 4en1 - Empresas de Alimentos</h1>
       <p>Todo en un solo lugar.</p>
     </div>
