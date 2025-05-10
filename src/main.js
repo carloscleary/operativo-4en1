d957ffe6e5f65132aa9bf7ea1210c345a65d0aeb
carloscleary <58497957+carloscleary@users.noreply.github.com>
2025-05-09 21:58:39 -0400

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+// src/main.js
+document.addEventListener("DOMContentLoaded", function() {
+    const root = document.getElementById("root");
+    root.innerHTML = '<h1>Operativo 4en1 - Empresas de Alimentos</h1><p>Todo en un solo lugar.</p>';
+});
