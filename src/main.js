2df6c6fc4f062acd6f495b985b52f565e2fd3436
carloscleary <58497957+carloscleary@users.noreply.github.com>
2025-05-12 17:55:15 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,8 +5,5 @@ import App from "./App";
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null, React.createElement(App))
 );
-
