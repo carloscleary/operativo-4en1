d062269f03f27c7d94f2933860dcf86837f4297f
carloscleary <58497957+carloscleary@users.noreply.github.com>
2025-05-12 16:03:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './',
-  publicDir: 'public',
+  plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './public/index.html', // Asegúrate que está en public
+      input: './public/index.html',
     },
   },
-  plugins: [react()],
 });
